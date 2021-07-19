38ea4e9624d103d70efd085c462bc057f5064cdd
Mulugeta Engdaw <mengdaw@codeaurora.org>
2021-07-19 13:31:58 -0700

    Kalama-BU: disable dlkms
    Change-Id: I4f66b2ef8439706439362da7af11ee2cb7cc6fe5

--- kalama.mk
+++ kalama.mk
@@ -169,7 +169,7 @@ TARGET_KERNEL_VERSION := 5.10
 TARGET_USES_NEW_ION := true
 
 # Disable DLKM generation until build support is available
-TARGET_KERNEL_DLKM_DISABLE := false
+TARGET_KERNEL_DLKM_DISABLE := true
 
 #####Dynamic partition Handling
 ###
