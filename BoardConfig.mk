9a35698ec14ac184740301710982113d656a4b96
Mulugeta Engdaw <mengdaw@codeaurora.org>
2021-08-11 15:35:18 -0700

    Kalama-BU: add androidboot.hardware to BOARD_KERNEL_CMDLINE
    once we consume the latest QSSI AUs we can move androidboot.hardware to
    BOARD_BOOTCONFIG
    
    Change-Id: I5ac44b3f3a85935a2337642423337a49f8e0013e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,8 +121,8 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API := true
 
-BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 androidboot.hardware=qcom
+BOARD_BOOTCONFIG := androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3
 
 # TARGET_CONSOLE_ENABLED allows to override the default kernel configuration
 # true  -- override kernel configuration to enable console
