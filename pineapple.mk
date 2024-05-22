634d6e8b0975e922c81eb28b7452976e165fb4ec
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-05-22 17:01:27 +0530

    xiaomi14: Ship fstab
    oops.
    
    Change-Id: Ib9e3004f808d4cf93e2080cc03cf1491fe3f39e0

--- pineapple.mk
+++ pineapple.mk
@@ -105,6 +105,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 PRODUCT_PACKAGES += \
+    fstab.qcom \
     init.target.rc \
     init.xiaomi.rc
 
