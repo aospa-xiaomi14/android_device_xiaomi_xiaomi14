eed1f1522909aa1dd8d232ed8e3e4cb6af66d2d0
Vaisakh Murali <mvaisakh@statixos.com>
2024-10-21 20:17:49 +0530

    xiaomi14: Move to blueprint package definition for init
    Change-Id: Idf81f8171ff247479f56b771ba2a2feea4a7e9a9

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -4,45 +4,9 @@
 
 LOCAL_PATH := $(call my-dir)
 
-#----------------------------------------------------------------------
-# Copy additional target-specific files
-#----------------------------------------------------------------------
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.target.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init/init.target.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.xiaomi.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init/init.xiaomi.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.qcom
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES := init/fstab.qcom
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := charger_fw_fstab.qti
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init/charger_fw_fstab.qti
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
 include device/qcom/vendor-common/MergeConfig.mk
 
 #----------------------------------------------------------------------
 # Configs common to AndroidBoard.mk for all targets
 #----------------------------------------------------------------------
 include vendor/qcom/opensource/core-utils/build/AndroidBoardCommon.mk
-
